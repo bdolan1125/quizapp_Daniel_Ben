1f479ed0fe76279990dad3e56490a9aab63c27fa
Benjamin Dolan <benjamin@thinkful.com>
2020-01-09 15:13:50 -0700

    finished stump functions and began coding

--- app.js
+++ app.js
@@ -1,103 +1,155 @@
 /**
  * Example store structure
  */
-const store = {
+//const store = {
   // 5 or more questions are required
-  questions: [
-    {
-      question: 'Which battle royale focuses on building?',
-      answers: [
-        'H1Z1',
-        'Apex Legends',
-        'Fortnite',
-        'Totally Accurate Battle Royale'
-      ],
-      correctAnswer: 'Fortnite'
-    },
-    {
-      question: 'What was the first battle royale to implement a respawn feature?',
-      answers: [
-        'Apex Legends',
-        'Fortnite',
-        'PUBG',
-        'Realm Royale'
-      ],
-      correctAnswer: 'Apex Legends'
-    },
-    {
-      question: 'In Player Unknown\'s BattleGrounds (PUBG) which melee weapon is the strongest?',
-      answers: [
-        'Baseball Bat',
-        'Machete',
-        'Crowbar',
-        'Frying Pan'
-      ],
-      correctAnswer: 'Frying Pan'
-    },
-    {
-      question: 'Which battle royale features all skins for free?',
-      answers: [
-        'Apex Legends',
-        'Totally Accurate Battle Royale',
-        'PUBG',
-        'Realm Royale'
-      ],
-      correctAnswer: 'Totally Accurate Battle Royale'
-    },
-    {
-      question: 'Which battle royale was the first to feature cross console play?',
-      answers: [
-        'Apex Legends',
-        'Fortnite',
-        'PUBG',
-        'Realm Royale'
-      ],
-      correctAnswer: 'Fortnite'
-    }
-  ],
-  questionNumber: 0,
-  score: 0
-};
+//   questions: [
+//     {
+//       question: 'Which battle royale focuses on building?',
+//       answers: [
+//         'H1Z1',
+//         'Apex Legends',
+//         'Fortnite',
+//         'Totally Accurate Battle Royale'
+//       ],
+//       correctAnswer: 'Fortnite'
+//     },
+//     {
+//       question: 'What was the first battle royale to implement a respawn feature?',
+//       answers: [
+//         'Apex Legends',
+//         'Fortnite',
+//         'PUBG',
+//         'Realm Royale'
+//       ],
+//       correctAnswer: 'Apex Legends'
+//     },
+//     {
+//       question: 'In Player Unknown\'s BattleGrounds (PUBG) which melee weapon is the strongest?',
+//       answers: [
+//         'Baseball Bat',
+//         'Machete',
+//         'Crowbar',
+//         'Frying Pan'
+//       ],
+//       correctAnswer: 'Frying Pan'
+//     },
+//     {
+//       question: 'Which battle royale features all skins for free?',
+//       answers: [
+//         'Apex Legends',
+//         'Totally Accurate Battle Royale',
+//         'PUBG',
+//         'Realm Royale'
+//       ],
+//       correctAnswer: 'Totally Accurate Battle Royale'
+//     },
+//     {
+//       question: 'Which battle royale was the first to feature cross console play?',
+//       answers: [
+//         'Apex Legends',
+//         'Fortnite',
+//         'PUBG',
+//         'Realm Royale'
+//       ],
+//       correctAnswer: 'Fortnite'
+//     }
+//   ],
+//   questionNumber: 0,
+//   score: 0
+// };
 
-renderStartPage()
-//Renders start page of Quiz App
-renderQuestionPage()
-//Renders question pages of Quiz App
-renderAnswerPageCorrect()
+function renderStartPage(){
+const startPageHtml = (`<h2>Welcome to the Battle Royale Quiz</h2>
+<button type="button" class="start-quiz">Start Quiz</button>`);
+$('main').html(startPageHtml);
+console.log(renderStartPage)
+}
+
+
+function renderQuestionPage(){
+  const questionPageHtml = (`<h2 class="question-number">Question X</h2>
+<p class="question-text">Question Text?</p>
+<form class="answer-list-form">
+<p class="answer-list">
+    <input type = "radio"
+                 name = "Answer1"
+                 id = "answerOne"
+                 value = "1"
+                 checked = "checked" />
+    <label for = "Answer1">blank</label>
+    <input type = "radio"
+                 name = "Answer2"
+                 id = "answerTwo"
+                 value = "2" />
+    <label for = "Answer2">blank</label>
+    <input type = "radio"
+                 name = "Answer3"
+                 id = "answerThree"
+                 value = "3" />
+    <label for = "Answer3">blank</label>
+    <input type = "radio"
+                 name = "Answer4"
+                 id = "answerFour"
+                 value = "4" />
+    <label for = "Answer4">blank</label>
+</p>
+</form>
+<button type="button" class="submit">Submit</button>`)
+$('main').html(questionPageHtml);
+console.log(renderQuestionPage)
+}
+//function renderAnswerPageCorrect()
 //Renders correct answer pages of quiz app
-renderAnswerPageIncorrect()
+
+//function renderAnswerPageIncorrect()
 //Renders incorrect answer pages of quiz app
-renderResultsPage()
+
+//function renderResultsPage()
 //Renders result page of quiz app
 
-quizStarter()
+function quizStarter(){
+  $('main').on('click 13', '.start-quiz', event => {
+    renderQuestionPage();
+  });
+}
+
 //listens for click events on start button
 //calls renderQuestionPage()
 
-answerSubmitter()
+//function answerSubmitter()
 //listens for submit events in form
 //calls answerChecker() with form value
 
-answerChecker()
+//function answerChecker()
 //receives value from answerSubmitter()
 //compares supplied value with correct value
 //if === calls renderAnswerPageCorrect()
 //else !== calls renderAnswerPageIncorrect()
 
-nextQuestion()
+//function nextQuestion()
 //listens for click events on next question button
 //calls renderQuestionPage()
 
 //LOOP ABOVE FUNCTIONS until STORE is exhausted
 
-playAgain()
+//function playAgain()
 //listens for click events on play again button
 //calls renderStartPage
 
-scoreCounter()
+//function scoreCounter()
 //refers to const which displays score
 //++ to score const upon call
 
+function handleQuizApp(){
+  renderStartPage();
+  quizStarter();
+  // nextQuestion();
+  // playAgain();
+  // answerChecker();
+  // answerSubmitter();
+}
+$(handleQuizApp);
 
 
 /**
